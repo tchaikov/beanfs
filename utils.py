b42b75259a17cb7587740d07295490a49bd481ec
yami <yami@yami-workshop.corp.emc.com>
2009-05-07 21:45:06 +0800

    add utils.find to find the 1st item in a sequence

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ def exists_by_property(model, prop, value):
     logging.debug('%d entries found with %s = %r' % (records.count(), prop, value))
     return records.count() > 0
 
+
 def get1_by_property(model, prop, value):
     records = model.all().filter('%s = ' % prop, value)
 
@@ -14,3 +15,14 @@ def get1_by_property(model, prop, value):
         return None
     else:
         assert 0, "%s has multiple records with %s=%s\n" % (model, prop, value)
+
+
+def find(pred, seq, ret=None):
+    """
+    Return first item in sequence where pred(item) is True.
+    """
+    for item in seq:
+        if pred(item):
+            return item
+
+    
